537984d654b3eb1e640b1f36eb4980642795b98b
Carlos Beuret <guillermobeuret@hotmail.com>
2023-05-10 12:46:37 -0300

    ejercicios terminados unidad 1

--- Modulo_1/Unidad_1/sys_1.py
+++ Modulo_1/Unidad_1/sys_1.py
@@ -1,3 +1,11 @@
 import sys
 
-print(sys.path)
+
+if int(sys.argv[1]) %2 == 0:
+    print(sys.argv[1])
+
+if int(sys.argv[2]) %2 == 0:
+    print(sys.argv[2])
+
+if int(sys.argv[3]) %2 == 0:
+    print(sys.argv[3])
