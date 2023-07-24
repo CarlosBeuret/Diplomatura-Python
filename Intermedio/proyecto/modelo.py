0dd8a91d3d296f0d9008a83f6f58543e5c097531
Carlos Beuret <guillermobeuret@hotmail.com>
2023-07-24 17:42:22 -0300

    se separa la vista del modelo en el proyecto

--- Intermedio/proyecto/modelo.py
+++ Intermedio/proyecto/modelo.py
@@ -1,34 +1,10 @@
-from tkinter import * 
-from tkinter import ttk
-from tkinter import Label
-from tkinter import messagebox
-from datetime import datetime
 import sqlite3
+from tkinter import messagebox
 import re
-
-
-master = Tk()
+from datetime import datetime
 
 database_path = "database.db"
 con = sqlite3.connect(database_path)
-
-
-var_titulo = StringVar()
-var_editorial = StringVar()
-var_nombre = StringVar()
-
-lbl_regreso = Label(master, text="")
-lbl_regreso.config(text="El libro se regreso con exito.")
-
-lbl_guardar = Label(master, text="")
-lbl_guardar.config(text="El libro se ha agregado correctamente.")
-
-lbl_borrar = Label(master, text="")
-lbl_borrar.config(text="El libro se ha borrado correctamente.")
-
-lbl_prestamo = Label(master, text="")
-lbl_prestamo.config(text="El prestamo se ha realizado correctamente.")
-
 
 def crear_tabla():
     con = sqlite3.connect(database_path)
@@ -45,37 +21,7 @@ def crear_tabla():
     con.commit()
     con.close()
 
-
-def obtener_datos():
-    con = sqlite3.connect(database_path)
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM biblioteca")
-    datos = cursor.fetchall()
-    con.close()
-    return datos
-
-
-def mostrar_tabla():
-    datos = obtener_datos()
-    tree.delete(*tree.get_children())
-    for fila in datos:
-        tree.insert("", "end", values=fila)
-
-def libros_prestados():
-    con = sqlite3.connect(database_path)
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM biblioteca WHERE estado = 'Prestado'")
-    datos = cursor.fetchall()
-    con.close()
-    return datos
-
-def mostrar_prestados():
-    datos = libros_prestados()
-    tree.delete(*tree.get_children())
-    for fila in datos:
-        tree.insert("", "end", values=fila)
-
-def guardar():
+def guardar(var_titulo, var_editorial, lbl_guardar, tree):
     if var_titulo.get() == "" or var_editorial.get() == "" :
         messagebox.showinfo("Complete datos", "Para realizar un ingreso debe completar titulo y editorial")
     else:
@@ -88,11 +34,15 @@ def guardar():
         con.close()
         var_editorial.set("")
         var_titulo.set("")
-        oculta_mensaje()        
-        lbl_guardar.grid(column=1)
-        mostrar_tabla()
+        oculta_mensaje_guardar(lbl_guardar)        
+        
+        mostrar_tabla(tree)
 
-def borrar():
+def oculta_mensaje_guardar(lbl_guardar):
+    lbl_guardar.grid_forget()
+    lbl_guardar.grid(column=1)
+
+def borrar(tree, lbl_borrar):
     items = tree.focus()
     values = tree.item(items, "values")
     if len(values)== 0:
@@ -105,11 +55,14 @@ def borrar():
         cursor.execute(sql, (id,))
         con.commit()
         con.close()
-        oculta_mensaje()
+        oculta_mensaje_borrar(lbl_borrar)
         lbl_borrar.grid(column=1)
-        mostrar_tabla()
-   
-def prestamo():
+        mostrar_tabla(tree)
+
+def oculta_mensaje_borrar(lbl_borrar):
+    lbl_borrar.grid_forget()
+
+def prestamo(tree, var_nombre, lbl_prestamo):
     items = tree.focus()
     values = tree.item(items, "values")
     
@@ -132,13 +85,15 @@ def prestamo():
             cursor.execute(sql, ("Prestado", var_nombre.get(), var_fecha, id))
             con.commit()
             con.close()
-            oculta_mensaje()
+            oculta_mensaje_prestamo(lbl_prestamo)
             lbl_prestamo.grid(column=1)
-            mostrar_tabla()
+            mostrar_tabla(tree)
             var_nombre.set("")
-    
 
-def devolucion():
+def oculta_mensaje_prestamo(lbl_prestamo):
+      lbl_prestamo.grid_forget()
+
+def devolucion(tree, lbl_regreso):
     items = tree.focus()
     values = tree.item(items, "values")
    
@@ -172,71 +127,37 @@ def devolucion():
                 cursor.execute(sql, ("Sin prestamo", "", "Null", id))
                 con.commit()
                 con.close()
-                oculta_mensaje()
+                oculta_mensaje_regreso(lbl_regreso)
                 lbl_regreso.grid(column=1)
-                mostrar_tabla()
-  
-def oculta_mensaje():
+                mostrar_tabla(tree)
+
+def oculta_mensaje_regreso(lbl_regreso):
     lbl_regreso.grid_forget()
-    lbl_prestamo.grid_forget()
-    lbl_borrar.grid_forget()
-    lbl_guardar.grid_forget()
 
-tree = ttk.Treeview(master)
-tree["columns"] = ("col1", "col2", "col3","col4", "col5", "col6")
-tree.column("#0", width= 30, minwidth=50, anchor=W)
-tree.column("col1", width= 80, minwidth=80, anchor=W)
-tree.column("col2", width= 80, minwidth=80, anchor=W)
-tree.column("col3", width= 100, minwidth=100, anchor=W)
-tree.column("col4", width= 100, minwidth=100, anchor=W)
-tree.column("col5", width= 100, minwidth=100, anchor=W)
-tree.column("col6", width= 100, minwidth=100, anchor=W)
+def obtener_datos():
+    con = sqlite3.connect(database_path)
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM biblioteca")
+    datos = cursor.fetchall()
+    con.close()
+    return datos
 
-tree.heading("col1", text="ID")
-tree.heading("col2", text="Titulo")
-tree.heading("col3", text="Editorial")
-tree.heading("col4", text="Estado") 
-tree.heading("col5", text="Nombre")
-tree.heading("col6", text="Fecha")
-tree.grid(column=0, row=6, columnspan=6)
+def mostrar_tabla(tree):
+    datos = obtener_datos()
+    tree.delete(*tree.get_children())
+    for fila in datos:
+        tree.insert("", "end", values=fila)
 
-boton_guardar = Button(master, text="Guardar", command= guardar) 
-boton_guardar.grid(row=0, column=0)
+def libros_prestados():
+    con = sqlite3.connect(database_path)
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM biblioteca WHERE estado = 'Prestado'")
+    datos = cursor.fetchall()
+    con.close()
+    return datos
 
-
-boton_borrar = Button(master, text="Borrar", command= borrar) 
-boton_borrar.grid(row=1, column=0)
-
-boton_prestamo = Button(master, text="Prestamo", command= prestamo) 
-boton_prestamo.grid(row=0, column=2) 
-
-boton_devolucion = Button(master, text="Devolucion", command= devolucion) 
-boton_devolucion.grid(row=1, column=2) 
-
-boton_libros_prestados = Button(master, text="Libros prestados", command= mostrar_prestados) 
-boton_libros_prestados.grid(row=0, column=4) 
-
-boton_libros_totales = Button(master, text="Mostrar todo", command= mostrar_tabla) 
-boton_libros_totales.grid(row=1, column=4) 
-mostrar_tabla()
-
-
-titulo = Label(master, text = "Titulo ")
-titulo.grid(row=2, column= 0, sticky= W)
-editorial = Label(master, text = "Editorial", )
-editorial.grid(row=3, column= 0, sticky= W) 
-
-entry_titulo = Entry(master, textvariable= var_titulo, width= 30) 
-entry_titulo.grid(row=2, column= 1)
-entry_editorial = Entry(master, textvariable= var_editorial, width= 30)
-entry_editorial.grid(row=3, column= 1)
-
-
-nombre = Label(master, text = "Nombre ")
-nombre.grid(row=2, column= 2, sticky=W)
-
-
-entry_nombre = Entry(master, textvariable= var_nombre, width= 30) 
-entry_nombre.grid(row=2, column=3)
-
-master.mainloop()
+def mostrar_prestados(tree):
+    datos = libros_prestados()
+    tree.delete(*tree.get_children())
+    for fila in datos:
+        tree.insert("", "end", values=fila)
